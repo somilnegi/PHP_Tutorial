1b035a65515bc20e4a827af7e2509f5926ed3efb
Somil Negi <somilnegi12345@gmail.com>
2025-05-28 23:21:53 +0530

    add php basics file

---
+++ 01_Basics.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Tutorial</title>
+</head>
+<body>
+    <div class="container">
+        <!-- This is my first php website -->
+        <?php
+            // echo "Hello world, this is printed using PHP";
+            // $var1=5;
+            // $var2=2;
+            // echo $var1;
+            // echo "<br>";
+            // echo $var2;
+            // echo "<br>";
+        ?>
+
+        <?php
+
+            // Operators in PHP
+
+                // Arithmetic
+
+                // echo "The value of var1 + var2 is ";
+                // echo $var1+$var2;
+                // echo "<br>";
+                // echo "The value of var1 - var2 is ";
+                // echo $var1-$var2;
+                // echo "<br>";
+                // echo "The value of var1 * var2 is ";
+                // echo $var1*$var2;
+                // echo "<br>";
+                // echo "The value of var1 / var2 is ";
+                // echo $var1/$var2;
+                // echo "<br>";
+
+                // Assignment
+
+                // $newVar=$var1;
+                // echo "The value of new variable is ";
+                // $newVar+=2;
+                // $newVar-=2;
+                // $newVar*=2;
+                // $newVar/=2;
+                // echo $newVar;
+                // echo "<br>";
+
+                // Comparision
+
+                // echo "<h1> Comparision Operators</h1>"
+                // echo "The value of 1==4 is ";
+                // echo var_dump(1==4);
+                // echo "<br>";
+                // echo "The value of 1!=4 is ";
+                // echo var_dump(1!=4);
+                // echo "<br>";
+                // echo "The value of 1>=4 is ";
+                // echo var_dump(1>=4);
+                // echo "The value of 1<=4 is ";
+                // echo "<br>";
+                // echo var_dump(1<=4);
+                // echo "<br>";
+                
+                // Increment/ Decrement
+                
+                // echo $var1++;
+                // echo "<br>";
+                // echo $var1--;
+                // echo "<br>";
+                // echo ++$var1;
+                // echo "<br>";
+                // echo --$var1;
+                // echo "<br>";
+
+                // Logical
+
+                // $myVar=(true) and (true);
+                // $myVar=(false) and (true);
+                // $myVar=(false) and (false);
+                // echo var_dump($myVar);
+                // $myVar=(true) or (true);
+                // $myVar=(true) or (false);
+                // $myVar=(false) or (false);
+                // echo var_dump($myVar);
+                // $myVar=(true xor true);
+                // $myVar=(true) xor (false);
+                // $myVar=(false) xor (false);
+                // echo var_dump($myVar);
+   
+        ?>    
+        
+        <?php
+            // define('PI', 3.14);
+            // Data Types
+
+                // String
+
+                // $var="This is a string";
+                // echo var_dump($var);
+                // echo "<br>";
+                
+                // Integer
+
+                // $var=67;
+                // echo var_dump($var);
+                // echo "<br>";
+
+                // Float
+                
+                // $var=67.9;
+                // echo var_dump($var);
+                // echo "<br>";
+                
+                
+                // Boolean
+                
+                // $var=true;
+                // echo var_dump($var);
+                // echo "<br>";
+
+                // Array
+
+                // $languages=array("Python", "C++", "PHP", "NodeJS");
+                // echo $languages[2];
+                // echo count($languages);
+
+                // Object
+
+
+
+            // Constant
+            // echo PI;
+        ?>
+        
+        <?php
+            
+        ?>
+
+    </div>
+</body>
+</html>                                 
