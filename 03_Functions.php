4b1eeab28b472654fc1f10764f87755e0c8fc010
Somil Negi <somilnegi12345@gmail.com>
2025-05-29 16:46:13 +0530

    add functions

---
+++ 03_Functions.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Tutorial</title>
+</head>
+<body>
+    <h1>Learn Functions in PHP</h1>
+    <?php
+
+        // function print5() {
+        //     echo "FIVE";
+        // }
+        // print5();
+
+        function print_number($number) {
+            echo "Your number is ";
+            echo $number;
+        }
+        print_number(45);
+    ?>
+</body>
+</html>
